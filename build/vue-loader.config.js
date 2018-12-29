144cf1cdc4fa53b9150c1ffb3a202c57e43ea06b
赵灵霄.鼎阅智能.东升科技 <170377@dingyue.com>
2018-12-29 15:35:15 +0800

    cssmodule配置

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -2,7 +2,10 @@ module.exports = (isDev) => {
   return {
     preserveWhitepace: true,
     extractCSS: !isDev, // 生产环境要把.vue中的css打包进单独的css文件
-    cssModules: {},
+    cssModules: {
+      localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
+      camelCase: true
+    },
     // hotReload: false // 根据环境变量生成
   } 
 }
